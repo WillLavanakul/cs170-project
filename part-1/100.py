54dc0db9df49e477e9d0f25e28515d48db3c06c3
Will Lavanakul <willlvnkl@gmail.com>
2021-11-22 13:14:31 -0800

    part-1 scripts and files

---
+++ part-1/100.py
@@ -0,0 +1,28 @@
+import random
+
+file = open("100.in","r+")
+file.truncate(0)
+
+n = random.randint(76, 100)
+file.write(str(n) + "\n")
+for i in range(1, n+1):
+    file.write(str(i) + " " + str(random.randint(1, 1440)) + " " + str(random.randint(1, 60)) + " " +  str(random.randint(1, 99)) + "\n")
+file.close()
+
+file = open("150.in","r+")
+file.truncate(0)
+
+n = random.randint(101, 150)
+file.write(str(n) + "\n")
+for i in range(1, n+1):
+    file.write(str(i) + " " + str(random.randint(1, 1440)) + " " + str(random.randint(1, 60)) + " " +  str(random.randint(1, 99)) + "\n")
+file.close()
+
+file = open("200.in","r+")
+file.truncate(0)
+
+n = random.randint(151, 200)
+file.write(str(n) + "\n")
+for i in range(1, n+1):
+    file.write(str(i) + " " + str(random.randint(1, 1440)) + " " + str(random.randint(1, 60)) + " " +  str(random.randint(1, 99)) + "\n")
+file.close()

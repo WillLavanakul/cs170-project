66ecff455ddacfe4241051736c4c667ea31595e6
Will Lavanakul <willlvnkl@gmail.com>
2021-11-22 14:26:00 -0800

    last numbers to doubles

--- part-1/inputs.py
+++ part-1/inputs.py
@@ -6,7 +6,7 @@ file.truncate(0)
 n = random.randint(76, 100)
 file.write(str(n) + "\n")
 for i in range(1, n+1):
-    file.write(str(i) + " " + str(random.randint(1, 1440)) + " " + str(random.randint(1, 60)) + " " +  str(random.randint(1, 99)) + "\n")
+    file.write(str(i) + " " + str(random.randint(1, 1440)) + " " + str(random.randint(1, 60)) + " " +  str(round(random.uniform(1, 99), 3)) + "\n")
 file.close()
 
 file = open("150.in","r+")
@@ -15,7 +15,7 @@ file.truncate(0)
 n = random.randint(101, 150)
 file.write(str(n) + "\n")
 for i in range(1, n+1):
-    file.write(str(i) + " " + str(random.randint(1, 1440)) + " " + str(random.randint(1, 60)) + " " +  str(random.randint(1, 99)) + "\n")
+    file.write(str(i) + " " + str(random.randint(1, 1440)) + " " + str(random.randint(1, 60)) + " " +  str(round(random.uniform(1, 99), 3)) + "\n")
 file.close()
 
 file = open("200.in","r+")
@@ -24,5 +24,5 @@ file.truncate(0)
 n = random.randint(151, 200)
 file.write(str(n) + "\n")
 for i in range(1, n+1):
-    file.write(str(i) + " " + str(random.randint(1, 1440)) + " " + str(random.randint(1, 60)) + " " +  str(random.randint(1, 99)) + "\n")
+    file.write(str(i) + " " + str(random.randint(1, 1440)) + " " + str(random.randint(1, 60)) + " " +  str(round(random.uniform(1, 99), 3)) + "\n")
 file.close()

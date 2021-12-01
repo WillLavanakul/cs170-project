c3b562e7311a56dbdbe5bb7e01ce5c061f25ec5f
Darren Wong <darrenwong00@berkeley.edu>
2021-11-30 20:27:24 -0800

    Creating P matrix and initializing mem

--- part-1/inputs.py
+++ part-1/inputs.py
@@ -11,6 +11,15 @@ for i in range(1, n+1):
     file.write(str(i) + " " + str(random.randint(1, 1440)) + " " + str(random.randint(1, 60)) + " " +  str(round(random.uniform(1, 99), 3)) + "\n")
 file.close()
 
+file = open("test.in","r+")
+file.truncate(0)
+
+n = 2
+file.write(str(n) + "\n")
+for i in range(1, n+1):
+    file.write(str(i) + " " + str(random.randint(1, 1440)) + " " + str(random.randint(1, 60)) + " " +  str(round(random.uniform(1, 99), 3)) + "\n")
+file.close()
+
 file = open("150.in","r+")
 file.truncate(0)
 
--- solver.py
+++ solver.py
@@ -8,6 +8,23 @@ def solve(tasks):
     Returns:
         output: list of igloos in order of polishing  
     """
+    # Profit matrix; assumes deadlines are all 1440.
+    P = []
+    mem = []
+    for row in range (0, len(tasks)):
+        profits = []
+        hate = []
+        for col in range (0, 1441):
+            hate.append(float('-inf'))
+            t = tasks[row]
+            if col < t.get_duration():
+                profits.append(0)
+            elif col >= t.get_duration() and col <= t.get_deadline():
+                profits.append(t.get_max_benefit())
+            else:
+                profits.append(t.get_late_benefit(col - t.get_deadline()))
+        P.append(profits)
+        mem.append(hate)
     pass
 
 
@@ -15,9 +32,11 @@ def solve(tasks):
 
 # Here's an example of how to run your solver.
 if __name__ == '__main__':
-    for input_path in os.listdir('inputs'):
-        for file_name in os.listdir('inputs/' + input_path):
-            output_path = 'outputs/' + file_name + '.out'
-            tasks = read_input_file('inputs/' + input_path + "/" + file_name)
-            output = solve(tasks)
+    tasks = read_input_file('part-1/test.in')
+    output = solve(tasks)
+    #for input_path in os.listdir('inputs'):
+     #   for file_name in os.listdir('inputs/' + input_path):
+      #      output_path = 'outputs/' + file_name + '.out'
+       #     tasks = read_input_file('inputs/' + input_path + "/" + file_name)
+        #    output = solve(tasks)
             #write_output_file(output_path, output)

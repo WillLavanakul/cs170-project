8bf84b02768fd6d3f7949e19a6de6282a826698b
Will Lavanakul <willlvnkl@gmail.com>
2021-11-30 23:52:27 -0800

    1st version, pretty bad lmao

--- solver.py
+++ solver.py
@@ -8,14 +8,28 @@ def solve(tasks):
     Returns:
         output: list of igloos in order of polishing  
     """
+    # n = len(tasks)
+    # mem = [[0] + [-float('inf')]*1440]*n
+    # P = []
+    # for i in range(n):
+    #     row = []
+    #     task = tasks[i]
+    #     for j in range(0, 1441):
+    #         if j > 0 and j < task.duration:
+    #             row.append(0)
+    #         elif j:
+    #             row.append(task.get_late_benefit(j-task.deadline))
+    #         else:
+    #             row.append(0)
+    #     print(row)
+    #     P.append(row)
+
     # Profit matrix; assumes deadlines are all 1440.
+    n = len(tasks)
     P = []
-    mem = []
-    for row in range (0, len(tasks)):
+    for row in range (0, n):
         profits = []
-        hate = []
         for col in range (0, 1441):
-            hate.append(float('-inf'))
             t = tasks[row]
             if col < t.get_duration():
                 profits.append(0)
@@ -24,19 +38,50 @@ def solve(tasks):
             else:
                 profits.append(t.get_late_benefit(col - t.get_deadline()))
         P.append(profits)
-        mem.append(hate)
-    pass
+    
+    mem = [[0]*1441]
+    largest = 0
+    largest_id = [0, 0]
+    for i in range(1, n+1):
+        task = tasks[i-1]
+        row = []
+        for t in range(1441):
+            t_prime = t - task.get_duration()
+            if t_prime < 0:
+                row.append(mem[i-1][t])
+            else:
+                profit = P[i-1][t]
+                new = max(mem[i-1][t], profit + mem[i-1][t_prime])
+                if new > largest:
+                    largest_id = [i, t]
+                largest = max(largest, new)
+                row.append(new)
+        mem.append(row)
+
+    S = []
+    def get_sequence(i, t):
+        if i == 0:
+            return
+        if mem[i][t] == mem[i-1][t]:
+            get_sequence(i-1, t)
+        else:
+            task = tasks[i-1]
+            t_prime = t - task.get_duration()
+            get_sequence(i-1, t_prime)
+            S.append(i)
+
+    get_sequence(largest_id[0], largest_id[1])
+    return S
 
 
 #change
 
 # Here's an example of how to run your solver.
 if __name__ == '__main__':
-    tasks = read_input_file('part-1/test.in')
-    output = solve(tasks)
-    #for input_path in os.listdir('inputs'):
-     #   for file_name in os.listdir('inputs/' + input_path):
-      #      output_path = 'outputs/' + file_name + '.out'
-       #     tasks = read_input_file('inputs/' + input_path + "/" + file_name)
-        #    output = solve(tasks)
-            #write_output_file(output_path, output)
+    for input_path in os.listdir('inputs'):
+       for file_name in os.listdir('inputs/' + input_path):
+            output_path = 'outputs/' + input_path + "/" + file_name[:len(file_name)-3] + '.out'
+            tasks = read_input_file('inputs/' + input_path + "/" + file_name)
+            print('inputs/' + input_path + "/" + file_name)
+            output = solve(tasks)
+            write_output_file(output_path, output)

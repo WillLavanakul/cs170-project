8555a38114a1545eef247c32f4bc401d73e80471
WillLavanakul <willlvnkl@gmail.com>
2021-12-04 22:09:00 -0800

    fixed learning, need to play with heuristics

--- solver.py
+++ solver.py
@@ -13,12 +13,12 @@ def solve(tasks):
     """
 
     n = len(tasks)
-    alpha = 100.0
-    beta = 100.0
-    gamma = 3.0
-    phi = 1
+    alpha = 1440.0
+    beta = 60.0
+    gamma = 1.0
+    phi = 2 # GREEDY PARAMATER
     lam = 1
-    phero = [[1 for i in range(n+1)] for j in range(n+1)]
+    phero = [[1.0 for i in range(n+1)] for j in range(n+1)]
     max_profit = 0
     max_path = []
     iter_profit_history = []
@@ -46,89 +46,76 @@ def solve(tasks):
             return self.time + task.get_duration() <= 1440 and not self.visited[task.get_task_id()-1]
 
         def get_weights(self):
-            """
-            Given current state of ant, return list of weights for choosing
-            next job. Can only move to jobs not done or still have time left
-            to do.
-            """
-            heuristics = []
-            sum_hueristics = 0
-            max_heuristic = 0
-            current_task_id = 0
-            if self.task != None:
-                current_task_id = self.task.get_task_id()
+            weights = []
+            max_weight = 0
+            if self.task == None:
+                current_id = 0
+            else:
+                current_id = self.task.get_task_id()
             for task in self.tasks:
                 if self.can_do_task(task):
-                    # Calculate numerator of probability with heuristic and pheromone
-                    profit = gamma * float(task.get_profit(self.time + task.get_duration()))
-                    duration = alpha/float(task.get_duration())
-                    deadline = beta/float(task.get_deadline())
-                    heuristic = profit*(phero[current_task_id][task.get_task_id()]**lam)
-                    #print(task.get_task_id(), heuristic)
-                    heuristics.append(heuristic)
-                    max_heuristic = max(max_heuristic, heuristic)
-                    sum_hueristics += heuristic
+                    profit = gamma * task.get_profit(self.time + task.get_duration())
+                    duration = beta / task.get_duration()
+                    deadline = alpha/ task.get_deadline()
+                    heuristic = profit*duration*deadline
+                    weights.append(heuristic)
+                    max_weight = max(heuristic, max_weight)
                 else:
-                    heuristics.append(0)
-            # Scale each probability with total hueristics and pheromone
-            #print(max_heuristic)
-            # if sum_hueristics == 0:
-            #     return heuristics
-            # for i in range(len(heuristics)):
-            #     heuristics[i] = (heuristics[i] / sum_hueristics)
-            return heuristics
-
-    def move_ants(ants, tasks):
-        done_weights = [0]*n
-        done_ants = []
-        p = phero
-        for i in range(len(ants)):
-            ant = ants[i]
-            weights = ant.get_weights()
-            if weights != done_weights:
-                next_task = random.choices(tasks, weights)[0]
-                id = next_task.get_task_id()
-                ant.task = next_task
-                ant.time += next_task.get_duration()
-                ant.profit += next_task.get_profit(ant.time)
-                ant.path.append(id)
-                ant.visited[id-1] = True
-            else:
-                ants[i] = Ant(n, tasks)
-                done_ants.append(ant)
-        return done_ants
-                    
-
-    def update_phero(ants):
-        local_profit = 0
-        local_path = []
-        if len(ants) > 0:
-            for i in range(n+1):
-                for j in range(n+1):
-                    phero[i][j] = 0.8*phero[i][j]
+                    weights.append(0)
+            if max_weight > 0:
+                for i in range(len(weights)):
+                    weights[i] = ((weights[i] / max_weight)**phi) * (phero[current_id][i+1]**lam)
+            return weights
+            
+    def update_phero(ants, t):
+        for i in range(n+1):
+            for j in range(n+1):
+                phero[i][j] *= 0.9
+        new_phero = phero[:]
+        best_ant = max(ants, key=lambda a:a.profit)
+        worst_ant = min(ants, key=lambda a:a.profit)
+        delta = best_ant.profit - worst_ant.profit
+        iter_path = best_ant.path
         for ant in ants:
             path = ant.path
             profit = ant.profit
-            if profit > local_profit:
-                local_profit = profit
-                local_path = path
-            print(path)
-            print(profit)
-            iter_profit_history.append(profit)
             for i in range(len(path)-1):
                 u = path[i]
                 v = path[i+1]
-                phero[u][v] += (profit/(max_profit+1))
-            ant.clear()
-        return local_profit, local_path
+                if delta:
+                    new_phero[u][v] = new_phero[u][v] + ((profit-worst_ant.profit)/delta)**10
+                else:
+                    new_phero[u][v] = new_phero[u][v] + 1
+        iter_profit_history.append(best_ant.profit)
+        print(best_ant.profit)
+        return new_phero
+
+    def move_ants(ants, tasks):
+        p = phero
+        done = [False]*len(ants)
+        done_weights = [0]*n
+        while False in done:
+            for ants_index in range(len(ants)):
+                ant = ants[ants_index]
+                weights = ant.get_weights()
+                if weights == done_weights:
+                    done[ants_index] = True
+                else:
+                    next_task = random.choices(tasks, weights)[0]
+                    ant.task = next_task
+                    ant.path.append(next_task.get_task_id())
+                    ant.time += next_task.get_duration()
+                    ant.profit += next_task.get_profit(ant.time)
+                    ant.visited[next_task.get_task_id()-1] = True
+
     
-    ants = [Ant(n, tasks) for j in range(25)]
-    for i in range(1000):
-        done_ants = move_ants(ants, tasks)
-        local_profit, local_path = update_phero(done_ants)
-        if local_profit > max_profit:
-                max_profit = local_profit
-                max_path = local_path
+    
+    for i in range(100):
+        ants = [Ant(n, tasks) for j in range(5)]
+        move_ants(ants, tasks)
+        phero = update_phero(ants, i)
+        if i == 20:
+            phero = [[1.0 for i in range(n+1)] for j in range(n+1)]
     plt.plot(iter_profit_history)
     plt.show()
     a = Ant(n, tasks)
@@ -138,7 +125,7 @@ def solve(tasks):
     return max_path
 
 if __name__ == '__main__':
-    tasks = read_input_file('inputs/large/large-16.in')
+    tasks = read_input_file('inputs/large/large-247.in')
     output = solve(tasks)
     # for input_path in os.listdir('inputs'):
     #    for file_name in os.listdir('inputs/' + input_path):
@@ -153,3 +140,4 @@ if __name__ == '__main__':
 # inputs/large/large-247.in
 # inputs/small/small-193.in
 # samples/100.in
+# inputs/medium/medium-192.in
